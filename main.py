0ec437fcecd7d01eb1a76c6ed7027660b682a13e
Bryce Allen <bja49@zips.uakron.edu>
2021-04-19 10:24:11 -0400

    Prompts for 4a-e entered.

--- main.py
+++ main.py
@@ -19,9 +19,131 @@ df_data['target'] = data['target']
 
 df_data.head()
 
-# Neural Network for Data
+# NUERAL NETWORK FOR DATA
+
+# Different Hidden Layers (1,2)
+clf = MLPClassifier(alpha=1e-5,
+                    hidden_layer_sizes=(1,2), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# One Hidden Layer (5)
+clf = MLPClassifier(alpha=1e-5,
+                    hidden_layer_sizes=(5), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# One Hidden Layer (10)
+clf = MLPClassifier(alpha=1e-5,
+                    hidden_layer_sizes=(10), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# Two Hidden Layer (5,10)
+clf = MLPClassifier(alpha=1e-5,
+                    hidden_layer_sizes=(5,10), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# Two Hidden Layer (10,5)
+clf = MLPClassifier(alpha=1e-5,
+                    hidden_layer_sizes=(10,5), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# Three Hidden Layer (5,10,5)
+clf = MLPClassifier(alpha=1e-5,
+                    hidden_layer_sizes=(5,10,5), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# Three Hidden Layer (10,20,5)
+clf = MLPClassifier(alpha=1e-5,
+                    hidden_layer_sizes=(10,20,5), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# Gradient Descent Solver (10,20,5) - lbfgs
 clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                    hidden_layer_sizes=(10,3,3), random_state=1)
+                    hidden_layer_sizes=(10,20,5), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# Gradient Descent Solver (10,20,5) - sgd
+clf = MLPClassifier(solver='sgd', alpha=1e-5,
+                    hidden_layer_sizes=(10,20,5), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# Gradient Descent Solver (10,20,5) - adam
+clf = MLPClassifier(solver='adam', alpha=1e-5,
+                    hidden_layer_sizes=(10,20,5), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# Activation Function (10,20,5) - logistic
+clf = MLPClassifier(activation='logistic', alpha=1e-5,
+                    hidden_layer_sizes=(10,20,5), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# Activation Function (10,20,5) - relu
+clf = MLPClassifier(activation='relu', alpha=1e-5,
+                    hidden_layer_sizes=(10,20,5), random_state=1)
+cv_score = cross_val_score(clf,
+                           X=df_data.iloc[:, :-1],
+                           y=df_data['target'],
+                           cv=5)
+plt.plot(cv_score);
+plt.show()
+
+# Different Regularization Parameter (0.01,0.1)
+clf = MLPClassifier(alpha=(0.01,0.1), 
+                    hidden_layer_sizes=(10,20,5), random_state=1)
 cv_score = cross_val_score(clf,
                            X=df_data.iloc[:, :-1],
                            y=df_data['target'],
